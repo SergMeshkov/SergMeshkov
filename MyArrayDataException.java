3da28111782ff2612206b11380f9d19ef2da21e3
sergey <wdsae333@mail.ru>
2022-01-16 22:34:43 +0400

    I understood what was in the lesson, but as it came to practice, I did not master it

--- MyArrayDataException.java
+++ MyArrayDataException.java
@@ -4,7 +4,6 @@ public class MyArrayDataException extends Exception {
     public int i;
     public int j;
 
-
     MyArrayDataException(int i, int j) {
         this.i = i;
         this.j = j;
--- MyArraySizeException.java
+++ MyArraySizeException.java
@@ -2,6 +2,5 @@ package ru.geekbrains.java2.lessons.HomeWork.DZ.DZ2;
 
 public class MyArraySizeException extends Exception {
 
-
     }
 
